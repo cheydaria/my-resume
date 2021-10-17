65272ca8d44ebffdb849adac9eb93e0b48d6b33a
Carmel <heydarianc@gmail.com>
2021-10-16 23:24:07 -0600

    4th commit
    Got it to actually work

--- src/index.js
+++ src/index.js
@@ -1,18 +1,28 @@
 import { useState } from "react";
 import ReactDOM from "react-dom";
 
-
 function MyResume() {
- 
+  const [res, setRes] = useState({
+    sectionHead: "",
+    sectionContent: ""
+  });
+  
   function AboutCont() {
     return (<>
         <h3>About Me</h3>
         <p>A recent MS graduate from the GIT program at Arizona State University. 
            <br/> I am passionate about cohesive and inclusive design. 
            <br/>I aim to include more users’ experiences to create robust products.</p>
-        </>);
+        </>)
 }
 
+const headAbt = () => {
+  setRes(previousState => {
+    return { ...previousState, sectionContent: <AboutCont/>
+   
+  }
+  });
+}
 
   return (
     <div id = "page">
@@ -21,11 +31,15 @@ function MyResume() {
           <h2>heydarianc@gmail.com • https://carmelheydarian.com </h2>
         </div>
         <div id ="buttons">
-        <button className = "btn" id = "aboutMe" onClick={AboutCont}>About Me</button>
+        <button className = "btn" id = "aboutMe" onClick={headAbt}>About Me</button>
         </div>
+        <div>
+          {res.sectionContent}
+          </div>
         </div>
-      
+     
   )
 }
+
 
 ReactDOM.render(<MyResume />, document.getElementById('root'));
